06fec7ca2cae1dcd7e78eaa10611085f8e50cf15
Carlos D <dario20049@icloud.com>
2024-08-27 15:31:39 +0900

    Create Custom Hook

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,14 @@
+import {useEffect, useState} from "react"; // Importing useEffect and useState from react
+
 const UseFetch = (url) => {
-    
-}
+    // Declaring a state variable data and setData
+    const [data, setData] = useState();
+    useEffect(() => {
+        // Fetching data from the url
+        fetch(url).then(response => response.json()) // Converting the response to JSON
+            .then(data => setData(data)) // Setting the data to the state variable
+    }, []) // End of useEffect
+    return data; // Returning the data
+}// End of UseFetch
 
 export default UseFetch
