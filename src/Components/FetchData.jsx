6d346d99b887997b1f350b6a7dc77c13252d962e
Carlos D <dario20049@icloud.com>
2024-08-27 16:46:08 +0900

    fixing error returning data

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import UseFetch from "./UseFetch.jsx";
 import './FetchData.css';
 
+// Creating a functional component named FetchData
 function FetchData ()  {
 
     // Using the UseFetch custom hook to fetch data from the URL and storing it in the data variable
@@ -12,13 +13,12 @@ function FetchData ()  {
     return (
     <>
      <ul className='list_data_main'>
-        {/* Mapping through the data and displaying it in the list */}
-         {data && data.map(e => (
+         { data && data.map( e=> (
                 <>
                     <li className='list_data'>
                         <h3>{e.name}</h3>
                         <p><strong>Importance: </strong>{e.importance}</p>
-                        <p><strong>Benefits: </strong>{e.benefits6}</p>
+                        <p><strong>Benefits: </strong>{e.benefits}</p>
                         <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
                     </li>
                 </>
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,14 +1,18 @@
-import {useEffect, useState} from "react"; // Importing useEffect and useState from react
+import React, { useEffect, useState } from 'react'
 
+// Creating a custom hook named UseFetch
 const UseFetch = (url) => {
-    // Declaring a state variable data and setData
-    const [data, setData] = useState();
-    useEffect(() => {
-        // Fetching data from the url
-        fetch(url).then(response => response.json()) // Converting the response to JSON
-            .then(data => setData(data)) // Setting the data to the state variable
-    }, []) // End of useEffect
-    return data; // Returning the data
-}// End of UseFetch
+
+    // Using the useState hook to store the fetched data
+    const[data,setData]=useState();
+
+    // Using the useEffect hook to fetch data from the URL
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+            .then((data)=>setData(data))
+    },[]) // Passing an empty array as the second argument to useEffect to ensure that the fetch request is made only once
+    // Returning the fetched data
+    return [data]
+}
 
 export default UseFetch
